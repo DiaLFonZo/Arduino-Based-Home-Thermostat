406d258fd46b08fc8584c719da87457dba6e85fa
DiaLFonZo <dialfonzo@hotmail.com>
2017-10-21 12:37:51 -0400

    Add files via upload

---
+++ MikeRankin_SensorBoard_HelloWorld.ino
@@ -0,0 +1,19 @@
+
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define OLED_RESET 4
+Adafruit_SSD1306 display(OLED_RESET);
+
+void setup() {
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3c);  // initialize with the I2C addr 0x3D (for the 128x64)
+}
+
+void loop() {
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0,0);
+  display.println("Hello, world!");
+}
